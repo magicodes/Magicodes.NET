e6b818f9eda1a9cf372c657f5665b181a3fe9c29
magicodes <magicodes@sina.cn>
2014-12-29 18:17:55 +0800

    UserName

---
+++ Plus/CMS/Magicodes.CMS/Models/CMS_ContentTypes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Magicodes.CMS.Models
+{
+    /// <summary>
+    /// 内容类型
+    /// </summary>
+    public enum CMS_ContentTypes
+    {
+        Html,
+        Photo,
+        Video
+    }
+}
