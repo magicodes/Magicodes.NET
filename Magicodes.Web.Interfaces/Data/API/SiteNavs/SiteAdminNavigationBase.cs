7ca465b01e9c4a38875939bdc5a7550cae95c2fc
magicodes <magicodes@sina.cn>
2015-01-04 22:39:55 +0800

    SiteAdminMenus

---
+++ Magicodes.Web.Interfaces/Data/API/SiteNavs/SiteAdminNavigationBase.cs
@@ -0,0 +1,66 @@
+﻿using Magicodes.Web.Interfaces.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//======================================================================
+//
+//        Copyright (C) 2014-2016 Magicodes团队    
+//        All rights reserved
+//
+//        filename :ISiteNavigation
+//        description :
+//
+//        created by 雪雁 at  2014/12/30 16:47:03
+//        http://www.magicodes.net
+//
+//======================================================================
+namespace Magicodes.Web.Interfaces.Data.API.SiteNavs
+{
+    /// <summary>
+    /// 后台站点导航
+    /// </summary>
+    public class SiteAdminNavigationBase<TUserKeyType> : CommonBusinessModelBase<Guid, TUserKeyType>
+    {
+        /// <summary>
+        /// 父级Id
+        /// </summary>
+        public Guid? ParentId { get; set; }
+        /// <summary>
+        /// 显示名称
+        /// </summary>
+        [MaxLength(100)]
+        public string Text { get; set; }
+        /// <summary>
+        /// 菜单链接
+        /// </summary>
+        [MaxLength(300)]
+        public string Href { get; set; }
+        /// <summary>
+        /// 图标样式
+        /// </summary>
+        [MaxLength(256)]
+        public string IconCls { get; set; }
+        /// <summary>
+        /// 文本样式
+        /// </summary>
+        [MaxLength(256)]
+        public string TextCls { get; set; }
+        /// <summary>
+        /// 是否显示子级数或者通知数
+        /// </summary>
+        public bool isShowBadge { get; set; }
+        /// <summary>
+        /// 菜单右侧数字类型
+        /// </summary>
+        public MenuBadgeTypes MenuBadgeType { get; set; }
+        /// <summary>
+        /// 子级数或者通知数请求地址
+        /// </summary>
+        [MaxLength(300)]
+        public string BadgeRequestUrl { get; set; }
+    }
+}
---
+++ Magicodes.Web.Interfaces/Data/API/SiteNavs/SiteAdminNavigationRepositoryBase.cs
@@ -0,0 +1,50 @@
+﻿using Magicodes.Web.Interfaces.Data;
+using Magicodes.Web.Interfaces.Data.API.SiteNavs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//======================================================================
+//
+//        Copyright (C) 2014-2016 Magicodes团队    
+//        All rights reserved
+//
+//        filename :MessageDataRepositoryBase
+//        description :
+//
+//        created by 雪雁 at  2014/12/30 16:39:41
+//        http://www.magicodes.net
+//
+//======================================================================
+namespace Magicodes.Web.Interfaces.Data.API.SiteNavs
+{
+    public abstract class SiteAdminNavigationRepositoryBase<TUserKeyType> : IDataRepository<SiteAdminNavigationBase<TUserKeyType>>
+    {
+
+        public abstract IQueryable<SiteAdminNavigationBase<TUserKeyType>> GetQueryable();
+
+        public abstract IEnumerable<SiteAdminNavigationBase<TUserKeyType>> Get(System.Linq.Expressions.Expression<Func<SiteAdminNavigationBase<TUserKeyType>, bool>> filter = null, Func<IQueryable<SiteAdminNavigationBase<TUserKeyType>>, IOrderedQueryable<SiteAdminNavigationBase<TUserKeyType>>> orderBy = null, string includeProperties = "");
+
+        public abstract IEnumerable<SiteAdminNavigationBase<TUserKeyType>> Get(System.Linq.Expressions.Expression<Func<SiteAdminNavigationBase<TUserKeyType>, bool>> filter = null);
+
+        public abstract IPagedList<SiteAdminNavigationBase<TUserKeyType>> Get(System.Linq.Expressions.Expression<Func<SiteAdminNavigationBase<TUserKeyType>, bool>> filter = null, Func<IQueryable<SiteAdminNavigationBase<TUserKeyType>>, IOrderedQueryable<SiteAdminNavigationBase<TUserKeyType>>> orderBy = null, string includeProperties = "", int pageSize = 10, int pageIndex = 1);
+
+        public abstract SiteAdminNavigationBase<TUserKeyType> Get(params dynamic[] keyValues);
+
+        public abstract SiteAdminNavigationBase<TUserKeyType> GetByID(dynamic id);
+
+        public abstract void Add(SiteAdminNavigationBase<TUserKeyType> entity);
+
+        public abstract void Remove(dynamic id);
+
+        public abstract void Remove(SiteAdminNavigationBase<TUserKeyType> entityToDelete);
+
+        public abstract void RemoveRange(IEnumerable<SiteAdminNavigationBase<TUserKeyType>> entitys);
+
+        public abstract void Update(SiteAdminNavigationBase<TUserKeyType> entityToUpdate);
+
+        public abstract void SaveChanges();
+    }
+}
