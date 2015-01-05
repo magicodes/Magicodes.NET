55d1d048a1bd5e83d7b112543a3fff36a08c9ecf
magicodes <magicodes@sina.cn>
2015-01-05 11:05:13 +0800

    commit

---
+++ Models/Magicodes.Models.Mvc/DAL/SiteAdminNavigationRepository.cs
@@ -0,0 +1,145 @@
+﻿using Magicodes.Models.Mvc.Models;
+using Magicodes.Web.Interfaces.Data;
+using Magicodes.Web.Interfaces.Data.API.SiteNavs;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//======================================================================
+//
+//        Copyright (C) 2014-2016 Magicodes团队    
+//        All rights reserved
+//
+//        filename :SiteAdminNavigationRepository
+//        description :
+//
+//        created by 雪雁 at  2014/12/31 15:46:44
+//        http://www.magicodes.net
+//
+//======================================================================
+namespace Magicodes.Models.Mvc.DAL
+{
+    public class SiteAdminNavigationRepository : SiteAdminNavigationRepositoryBase<string>
+    {
+        internal DbContext context;
+        internal DbSet<SiteAdminNavigation> dbSet;
+        public SiteAdminNavigationRepository(DbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<SiteAdminNavigation>();
+        }
+        public override IQueryable<SiteAdminNavigationBase<string>> GetQueryable()
+        {
+            return dbSet.AsQueryable();
+        }
+
+        public override IEnumerable<SiteAdminNavigationBase<string>> Get(System.Linq.Expressions.Expression<Func<SiteAdminNavigationBase<string>, bool>> filter = null, Func<IQueryable<SiteAdminNavigationBase<string>>, IOrderedQueryable<SiteAdminNavigationBase<string>>> orderBy = null, string includeProperties = "")
+        {
+            IQueryable<SiteAdminNavigationBase<string>> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                return orderBy(query).ToList();
+            }
+            else
+            {
+                return query.ToList();
+            }
+
+        }
+
+        public override IEnumerable<SiteAdminNavigationBase<string>> Get(System.Linq.Expressions.Expression<Func<SiteAdminNavigationBase<string>, bool>> filter = null)
+        {
+            IQueryable<SiteAdminNavigationBase<string>> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return query.ToList();
+        }
+
+        public override Web.Interfaces.Data.IPagedList<SiteAdminNavigationBase<string>> Get(System.Linq.Expressions.Expression<Func<SiteAdminNavigationBase<string>, bool>> filter = null, Func<IQueryable<SiteAdminNavigationBase<string>>, IOrderedQueryable<SiteAdminNavigationBase<string>>> orderBy = null, string includeProperties = "", int pageSize = 10, int pageIndex = 1)
+        {
+            IQueryable<SiteAdminNavigationBase<string>> query = dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split
+                (new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+                query = orderBy(query);
+            //计算总记录数
+            var totalCount = query.Count();
+            //获取分页结果
+            var results = query.Skip((pageIndex - 1) * pageSize)
+                               .Take(pageSize)
+                               .ToList();
+            //返回分页列表
+            return new PagedList<SiteAdminNavigationBase<string>>(results, pageIndex, pageSize, totalCount);
+        }
+
+        public override SiteAdminNavigationBase<string> Get(params dynamic[] keyValues)
+        {
+            return dbSet.Find(keyValues);
+        }
+
+        public override SiteAdminNavigationBase<string> GetByID(dynamic id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public override void Add(SiteAdminNavigationBase<string> entity)
+        {
+            dbSet.Add(entity as SiteAdminNavigation);
+        }
+
+        public override void Remove(dynamic id)
+        {
+            var entityToDelete = dbSet.Find(id);
+            Remove(entityToDelete);
+        }
+
+        public override void Remove(SiteAdminNavigationBase<string> entityToDelete)
+        {
+            dbSet.Remove(entityToDelete as SiteAdminNavigation);
+        }
+
+        public override void RemoveRange(IEnumerable<SiteAdminNavigationBase<string>> entitys)
+        {
+            dbSet.RemoveRange(entitys as IEnumerable<SiteAdminNavigation>);
+        }
+
+        public override void Update(SiteAdminNavigationBase<string> entityToUpdate)
+        {
+            dbSet.Attach(entityToUpdate as SiteAdminNavigation);
+            context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public override void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+    }
+}
