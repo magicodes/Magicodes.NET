e9249e9f8059ecf561ba525b2392798e7dd34b68
magicodes <magicodes@sina.cn>
2015-01-29 15:34:04 +0800

    HTMLHelper

---
+++ Plus/Magicodes.Admin/Helpers/HMTLHelperExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using System.Web.Optimization;
+
+namespace Magicodes.Admin
+{
+    public static class HMTLHelperExtensions
+    {
+        public static string IsSelected(this System.Web.Mvc.HtmlHelper html, string controller = null, string action = null)
+        {
+            string cssClass = "active";
+            string currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            string currentController = (string)html.ViewContext.RouteData.Values["controller"];
+
+            if (String.IsNullOrEmpty(controller))
+                controller = currentController;
+
+            if (String.IsNullOrEmpty(action))
+                action = currentAction;
+
+            return controller == currentController && action == currentAction ?
+                cssClass : String.Empty;
+        }
+
+        public static string PageClass(this HtmlHelper html)
+        {
+            string currentAction = (string)html.ViewContext.RouteData.Values["action"];
+            return currentAction;
+        }
+        public static string GetImageSrc(this HtmlHelper html, string str)
+        {
+            return str.Replace("~/", "/Magicodes.Admin/");
+        }
+
+    }
+}
